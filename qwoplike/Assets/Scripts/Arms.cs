ecfe7efdb5ef1b49928a86f48bf5a170b279fde8
edikanabia <49695530+edikanabia@users.noreply.github.com>
2023-02-13 04:08:32 -0500

    sprites added to assets folder, some sprites replaced in the editor

--- qwoplike/Assets/Scripts/Arms.cs
+++ qwoplike/Assets/Scripts/Arms.cs
@@ -7,25 +7,30 @@ public class Arms : MonoBehaviour
 
     HingeJoint2D hinge;
     public KeyCode hingeKey;
+    float motorSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-
         hinge = GetComponent<HingeJoint2D>();
+        motorSpeed = hinge.motor.motorSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetKeyDown(hingeKey))
         {
             hinge.useMotor = true;
+            hinge.motor.motorSpeed.Equals(motorSpeed);
             
         }
-        else if (Input.GetKeyUp(hingeKey))
+        if (Input.GetKeyUp(hingeKey))
         {
+
             hinge.useMotor = false;
+
         }
     }
 }
